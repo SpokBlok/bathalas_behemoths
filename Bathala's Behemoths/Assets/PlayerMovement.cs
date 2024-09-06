17b797f3ef7bf310d1e45db5d18c73fe6b3bf60e
Migs Atienza <160925292+SpokBlok@users.noreply.github.com>
2024-09-06 20:06:53 +0800

    Camera follows mouse, small move + rotate bugs

--- Bathala's Behemoths/Assets/PlayerMovement.cs
+++ Bathala's Behemoths/Assets/PlayerMovement.cs
@@ -6,11 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    private Vector2 move;
+    private Vector2 move, mouseLook;
+    private Vector3 rotationTarget;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
+    }
+
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+        mouseLook = context.ReadValue<Vector2>();
     }
 
     // Start is called before the first frame update
@@ -22,17 +28,37 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(mouseLook);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            // Check if the object hit has a BoxCollider
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                // Your hover logic here
+                Debug.Log("Mouse is hovering over: " + hit.collider.gameObject.name);
+            }
+            rotationTarget = hit.point;
+        }
         movePlayer();
     }
 
     public void movePlayer()
     {
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        var lookPos = rotationTarget - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
 
-        if (movement != Vector3.zero)
+        Vector3 aimDirection = new Vector3(rotationTarget.x, 0, rotationTarget.z);
+
+
+        if (aimDirection != Vector3.zero)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
         }
+
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }

9112948fbeacc7df8ac3a3874e63bb6b402b1692
SpokBlok <160925292+SpokBlok@users.noreply.github.com>
2024-11-04 17:19:00 +0800

    Added Display Skills logic for the right panel

--- Bathala's Behemoths/Assets/Scripts/BaseUpgradeUIPanel.cs
+++ Bathala's Behemoths/Assets/Scripts/BaseUpgradeUIPanel.cs
@@ -26,6 +26,21 @@ public class BaseUpgradeUIPanel : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    public void DisplaySkill1()
+    {
+        Transform panel = transform.Find("RightPanel");
+        foreach (Transform child in panel)
+        {
+            if (child.name == "Skill 1")
+            {
+                child.gameObject.SetActive(true);
+            }
+            else { 
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
     public void Skill1Upgrade()
     {
         PlayerStats.Instance.AddSpeed(10);

dfa12df5d7208008f33fa96850733ca411e1c749
SpokBlok <160925292+SpokBlok@users.noreply.github.com>
2025-01-08 17:18:20 +0800

    MC Skill Panel started on, skill 1 equip working

--- Bathala's Behemoths/Assets/Scripts/MC Skill Building/MCSkillsUIPanel.cs
+++ Bathala's Behemoths/Assets/Scripts/MC Skill Building/MCSkillsUIPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MCSkillsUIPanel : MonoBehaviour
 {
@@ -10,24 +11,43 @@ public class MCSkillsUIPanel : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void EnablePanel()
     {
         gameObject.SetActive(true);
+        EventManager.Instance.InvokeOnEnteringUpgradeScreen();
     }
 
     public void DisablePanel()
     {
         gameObject.SetActive(false);
+        EventManager.Instance.InvokeOnExitingUpgradeScreen();
         Transform panel = transform.Find("RightPanel");
         foreach (Transform child in panel)
         {
             child.gameObject.SetActive(false);
         }
+    }
+
+    public void DisplaySkill()
+    {
+        GameObject clickedObject = EventSystem.current.currentSelectedGameObject;
+        Transform panel = transform.Find("RightPanel");
+        foreach (Transform child in panel)
+        {
+            if (child.name == clickedObject.name)
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void Skill1Upgrade()
+    {
+        FirstAid firstAid = PlayerSkills.Instance.GetComponentInChildren<FirstAid>();
+        PlayerSkills.Instance.MainCharacterSkillChange(firstAid);
     }
 }

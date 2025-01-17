c218f79b1d0e46445823252ac0dbf40eca855891
SpokBlok <160925292+SpokBlok@users.noreply.github.com>
2025-01-18 06:45:54 +0800

    AOE Circle Attack created

--- Bathala's Behemoths/Assets/Scripts/FillEffect.cs
+++ Bathala's Behemoths/Assets/Scripts/FillEffect.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class FillEffect : MonoBehaviour
 {
+    private Transform fillCircle;
+    private float timer;
+    private Vector3 fillVector;
 
     void Start()
     {
-        
+        fillCircle = transform.Find("Inside Circle").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer <= 5f)
+        {
+            timer += Time.deltaTime;
+            fillCircle.localScale = new (timer + 1, timer + 1, timer*3);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }

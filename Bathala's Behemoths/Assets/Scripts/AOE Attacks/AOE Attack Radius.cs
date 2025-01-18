f84f07e96074d861cd24d4338d11283d9ad87547
SpokBlok <160925292+SpokBlok@users.noreply.github.com>
2025-01-18 16:01:34 +0800

    AOE Attack radius converted into an abstract class

--- Bathala's Behemoths/Assets/Scripts/AOE Attacks/AOE Attack Radius.cs
+++ Bathala's Behemoths/Assets/Scripts/AOE Attacks/AOE Attack Radius.cs
@@ -3,59 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class AOEAttackRadius : MonoBehaviour
+public abstract class AOEAttackRadius : MonoBehaviour
 {
-    public SphereCollider radiusCollider;
-    GameObject player;
-    private Vector3 sphereCenter;
-    private float sphereRadius;
-
-    public float attackDamage;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        float scale = transform.parent.GetComponentInChildren<FillEffect>().finalScale;
-        transform.localScale = new(scale, scale, scale);
-
-        radiusCollider = GetComponent<SphereCollider>();
-
-        player = GameObject.FindGameObjectWithTag("Player");
-        // Get the terrain height at the character's current position (X, Z)
-        float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position);
-        Vector3 position = transform.position;
-        transform.position = new(position.x, terrainHeight, position.z);
-
-        sphereCenter = radiusCollider.transform.position + radiusCollider.center;
-        sphereRadius = radiusCollider.radius * Mathf.Max(
-            radiusCollider.transform.lossyScale.x,
-            radiusCollider.transform.lossyScale.y,
-            radiusCollider.transform.lossyScale.z
-        );
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Damage()
-    {
-        // Detect all colliders within the sphere
-        Collider[] hitColliders = Physics.OverlapSphere(sphereCenter, sphereRadius);
-
-        // Process detected colliders
-        foreach (Collider hitCollider in hitColliders)
-        {
-            if (hitCollider.CompareTag("Player"))
-            {
-                hitCollider.GetComponent<PlayerMovement>().TakeDamage(attackDamage);
-            } 
-            else if (hitCollider.CompareTag("Enemy"))
-            {
-                hitCollider.GetComponent<EnemyMob>().TakeDamage(attackDamage);
-            }
-        }
-    }
+    public abstract void Damage();
 }

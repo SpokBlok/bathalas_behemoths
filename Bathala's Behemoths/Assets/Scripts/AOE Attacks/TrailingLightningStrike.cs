7e30382e8dc2d4541604a234a8884bccde4251ba
SpokBlok <160925292+SpokBlok@users.noreply.github.com>
2025-01-20 23:39:46 +0800

    Trailing Lightning Strike created

--- Bathala's Behemoths/Assets/Scripts/AOE Attacks/TrailingLightningStrike.cs
+++ Bathala's Behemoths/Assets/Scripts/AOE Attacks/TrailingLightningStrike.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class TrailingLightningStrike : MonoBehaviour
 {
-    public TambanokanoLightningStrike lightningPrefab;
+    public FillEffect lightningPrefab;
     private GameObject player;
+    private float timer = 0;
+    private float attacksLeft = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,18 @@ public class TrailingLightningStrike : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < 1.5f)
+            {
+            timer += Time.deltaTime;
+        }
+        else if (attacksLeft > 0)
+        {
+            Instantiate(lightningPrefab, player.transform.position, Quaternion.identity);
+            timer = 0f;
+            attacksLeft--;
+        } else
+        {
+            Destroy(gameObject);
+        }
     }
 }

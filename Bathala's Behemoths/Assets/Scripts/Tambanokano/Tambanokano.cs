6ee10e4a00972d7e38304a019d0ddd31be8a2d99
SpokBlok <160925292+SpokBlok@users.noreply.github.com>
2025-01-21 22:23:14 +0800

    Tambanokano final ult done

--- Bathala's Behemoths/Assets/Scripts/Tambanokano/Tambanokano.cs
+++ Bathala's Behemoths/Assets/Scripts/Tambanokano/Tambanokano.cs
@@ -11,37 +11,88 @@ public class Tambanokano : EnemyMob
     public GameObject clawSwipePrefab;
     public GameObject trailingLightningPrefab;
     public GameObject arenaWideLightingPrefab;
+    public GameObject massiveAOEPrefab;
 
-    private BoxCollider clawRange;
+    private Coroutine randomAttackCoroutine;
+    private bool isUlting;
 
     private bool isLightingStriking;
+    private bool stunned;
 
     public float health;
 
-    private int minutesPassed;
-    private int halfMinutesPassed;
+    public int attacksPassed;
 
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        clawRange = GetComponentInChildren<BoxCollider>();
         health = 5000f;
+
+        isLightingStriking = false;
+        stunned = false;
+
+        attacksPassed = 9;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (randomAttackCoroutine == null && !isUlting)
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
 
+            isLightingStriking = false;
+            int index = Random.Range(0, 3);
+            if (attacksPassed >= 10)
+            {
+                index = 3;
+            }
+            attacksPassed++;
+
+            switch(index)
+            {
+                case 0:
+                    randomAttackCoroutine = StartCoroutine(ClawSwipe());
+                    break;
+
+                case 1:
+                    randomAttackCoroutine = StartCoroutine(TrailingLightning());
+                    isLightingStriking = true;
+                    break;
+
+                case 2:
+                    randomAttackCoroutine = StartCoroutine(ArenaWideLightning());
+                    isLightingStriking = true;
+                    break;
+
+                case 3:
+                    randomAttackCoroutine = StartCoroutine(UltimateAttack());
+                    isLightingStriking = true;
+                    attacksPassed = 0;
+                    break;
+            }
+        }
     }
 
     public override IEnumerator Stun(float duration)
     {
         if (isLightingStriking)
         {
-            //cancel lightning strikes
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+            StopCoroutine(randomAttackCoroutine);
+            randomAttackCoroutine = null;
+            isLightingStriking = false;
+            isUlting = false;
         }
+        //stun animation
         yield return new WaitForSeconds(duration);
     }
 
@@ -54,21 +105,88 @@ public class Tambanokano : EnemyMob
         }
     }
 
-    private IEnumerator ClawSwipe()
+    private IEnumerator SingleClawSwipe()
     {
-        Instantiate(clawSwipePrefab, new(Random.Range(250f, 650f), 170f, 359f), Quaternion.Euler(0f, 90f, 0f));
-        yield return new WaitForSeconds(1);
+        //attack animation
+        GameObject claw = Instantiate(clawSwipePrefab, new Vector3(Random.Range(250f, 650f), 170f, 365f), Quaternion.Euler(0f, 90f, 0f));
+        claw.transform.parent = transform;
+        yield return new WaitForSeconds(claw.GetComponent<FillEffect>().attackDuration);
+        yield return new WaitForSeconds(0.5f);
     }
 
-    private IEnumerator TrailingLightning()
+    private IEnumerator ClawSwipe() //6 second claw swipe, 14 second total
     {
-        Instantiate(trailingLightningPrefab, new(0f, 0f, 0f), Quaternion.identity);
-        yield return new WaitForSeconds(3);  
+        //attack animation
+        GameObject claw = Instantiate(clawSwipePrefab, new Vector3(Random.Range(250f, 650f), 170f, 370f), Quaternion.Euler(0f, 90f, 0f));
+        claw.transform.parent = transform;
+        yield return new WaitForSeconds(claw.GetComponent<FillEffect>().attackDuration);
+        yield return new WaitForSeconds(0.5f);
+
+        //attack animation
+        claw = Instantiate(clawSwipePrefab, new Vector3(Random.Range(250f, 650f), 170f, 370f), Quaternion.Euler(0f, 90f, 0f));
+        yield return new WaitForSeconds(claw.GetComponent<FillEffect>().attackDuration);
+        yield return new WaitForSeconds(1.5f);
+
+        randomAttackCoroutine = null;
     }
 
-    private IEnumerator ArenaWideLightning()
+    private IEnumerator TrailingLightning() //6 * 1.5 seconds = 9 seconds total
     {
-        Instantiate(arenaWideLightingPrefab, player.transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(8);
+        isLightingStriking = true;
+        
+        GameObject lightning = Instantiate(trailingLightningPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        lightning.transform.parent = transform;
+        TrailingLightningStrike lightningComponent = lightning.GetComponent<TrailingLightningStrike>();
+        if (lightningComponent == null)
+        {
+            Debug.LogError("TrailingLightningStrike component not found on lightning prefab.");
+            yield break; // Exit the coroutine if the component is not found
+        }
+
+        float duration = lightningComponent.attackGapDuration * lightningComponent.attacksLeft;
+        
+        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(1.5f);
+
+        randomAttackCoroutine = null;
+    }
+
+    private IEnumerator ArenaWideLightning() //5 seconds
+    {
+        isLightingStriking = true;
+
+        GameObject lightning = Instantiate(arenaWideLightingPrefab, player.transform.position, Quaternion.identity);
+        lightning.transform.parent = transform;
+        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(1.5f);
+
+        randomAttackCoroutine = null;
+    }
+
+    private IEnumerator UltimateAttack()
+    {
+        isUlting = true;
+        StartCoroutine(TrailingLightning()); // 6*1.5 = 9 seconds
+        StartCoroutine(ArenaWideLightning()); //5 seconds
+        StartCoroutine(ClawSwipe()); // 12.5 seconds
+
+        yield return new WaitForSeconds(6);
+        StartCoroutine(ArenaWideLightning());
+
+        yield return new WaitForSeconds(4);
+        StartCoroutine(TrailingLightning());
+
+        yield return new WaitForSeconds(2);
+        StartCoroutine(SingleClawSwipe());
+        StartCoroutine(ArenaWideLightning());
+
+        yield return new WaitForSeconds(7);
+        GameObject lightning = Instantiate(massiveAOEPrefab, player.transform.position, Quaternion.identity);
+        lightning.transform.parent = transform;
+
+        yield return new WaitForSeconds(10);
+
+        isUlting = false;
+        randomAttackCoroutine = null;
     }
 }

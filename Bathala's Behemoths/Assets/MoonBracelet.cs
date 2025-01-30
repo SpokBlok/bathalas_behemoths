6a3d2bd9bbf193f0d4b1eeb7703924f9e59f6425
fusropatatas <mkdparpas@gmail.com>
2025-01-30 10:12:23 +0800

    Fully deployed clue3 fetch quest, only clue 5 (fetch) and clue 6 (linear story) quests remaining

---
+++ Bathala's Behemoths/Assets/MoonBracelet.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class MoonBracelet : MonoBehaviour
+{
+    public PlayerStats playerStats;
+    public TextMeshProUGUI popUp;
+    public GameObject dialogue;
+    public GameObject moonNPC;
+    public TheMoonDialogue moonDialogue;
+    
+    private bool isInTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        // Check for the key press only when inside the trigger
+        if (context.performed && isInTrigger)
+        {
+            moonNPC.SetActive(false);
+            moonDialogue.moonChunkGet = true;
+            dialogue.SetActive(true);
+        }
+    }
+
+    // Called when entering the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTrigger = true;
+            popUp.gameObject.SetActive(true);
+        }
+    }
+
+    // Called when exiting the trigger
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTrigger = false;
+            popUp.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Bathala's Behemoths/Assets/TheMoonDialogue.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TheMoonDialogue : MonoBehaviour
+{
+    public PlayerStats playerStats;
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public string[] linesRepeat;
+    public string[] linesRepeat2;
+    public string[] currentLines;
+    public float textInterval;
+    public GameObject pointer;
+    public GameObject clueImage;
+
+    private bool imageActive = false;
+    Vector3 currentPosition;
+    public bool dialogueRepeat;
+    public bool moonChunkGet;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        dialogueRepeat = false;
+        moonChunkGet = false;
+        currentPosition = gameObject.transform.localPosition;
+    }
+
+    void OnEnable()
+    {
+        if(dialogueRepeat && moonChunkGet)
+        {
+            currentLines = linesRepeat;
+        }
+        else if(dialogueRepeat)
+        {
+            currentLines = linesRepeat2;
+        }
+        else
+        {
+            currentLines = lines;
+        }
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(textComponent.text == currentLines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = currentLines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in currentLines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textInterval);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < currentLines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else if(imageActive == true)
+        {
+            gameObject.transform.localPosition = currentPosition;
+            gameObject.SetActive(false);
+            clueImage.SetActive(false);
+            pointer.SetActive(false);
+            imageActive = false;
+            
+            Debug.Log("inside end state");
+        }
+        else
+        {
+            gameObject.transform.localPosition = new Vector3 (1000, 1000);
+            if(dialogueRepeat && moonChunkGet)
+            {
+                playerStats.clue3 = true;
+                clueImage.SetActive(true);
+            }
+            else
+            {
+                pointer.SetActive(true);
+            }
+            imageActive = true;
+            dialogueRepeat = true;
+        }
+    }
+}

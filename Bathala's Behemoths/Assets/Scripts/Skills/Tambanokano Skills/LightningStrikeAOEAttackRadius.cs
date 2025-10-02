544058062cff168d99a683d833af44ca31f404c7
Migs Atienza <160925292+SpokBlok@users.noreply.github.com>
2025-10-02 15:20:22 +0800

    Finalized lightning strike

--- Bathala's Behemoths/Assets/Scripts/Skills/Tambanokano Skills/LightningStrikeAOEAttackRadius.cs
+++ Bathala's Behemoths/Assets/Scripts/Skills/Tambanokano Skills/LightningStrikeAOEAttackRadius.cs
@@ -13,39 +13,25 @@ public class LightningStrikeAOEAttackRadius : AOEAttackRadius
     // Start is called before the first frame update
     void Start()
     {
-        float scaleX = transform.parent.GetComponentInChildren<LightningStrikeFillEffect>().finalScaleX;
-        transform.localScale = new(scaleX, scaleX, scaleX);
-
         radiusCollider = GetComponent<SphereCollider>();
-
-        // Get the terrain height at the object's current position (X, Z)
-        float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position);
-        Vector3 position = transform.position;
-        transform.position = new(position.x, terrainHeight, position.z);
-
-        sphereCenter = radiusCollider.transform.position + radiusCollider.center;
         sphereRadius = radiusCollider.radius * Mathf.Max(
             radiusCollider.transform.lossyScale.x,
             radiusCollider.transform.lossyScale.y,
             radiusCollider.transform.lossyScale.z
         );
+
+        sphereCenter = radiusCollider.transform.position + radiusCollider.center;
     }
 
     public override void Damage()
     {
         Collider[] hitColliders = Physics.OverlapSphere(sphereCenter, sphereRadius);
-        attackDamage = PlayerStats.Instance.basicAttackDamage;
+        attackDamage = PlayerStats.Instance.basicAttackDamage * 0.7f;
         foreach (Collider hitCollider in hitColliders)
         {
             if (hitCollider.CompareTag("Enemy"))
             {
-                Debug.Log("LightningHitEnemy");
                 hitCollider.GetComponent<EnemyMob>().TakeDamage(attackDamage);
-            }
-            else if (hitCollider.CompareTag("Player"))
-            {
-                Debug.Log("LightningHitMe");
-                hitCollider.GetComponent<PlayerMovement>().TakeDamage(attackDamage);
             }
         }
     }
